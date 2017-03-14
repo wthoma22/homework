6c483837502fa459981381b8e67199421e56971a
wthoma22 <w.thomas390@yahoo.com>
2017-03-14 09:19:09 -0600

    first commit

---
+++ pseudocode.rb
@@ -0,0 +1,12 @@
+The customer representative (program) starts by saying, "Hello this is a Grocery store."
+If the customer (user) does not say anything (just hits enter) 
+  The the customer representative (program) responds with, "Hello."
+If the customer (user) speaks softly (types in all lower case)
+  The the customer representative (program) will respond with, "I AM HAVING A HARD TIME HEARING YOU."
+If the customer (user) speaks loudly (types in all upper case)
+  The the customer representative (program) will respond with, "NO, THIS IS NOT A PET STORE."
+If the customer (user) says, "GOODBYE!"
+  The the customer representative (program) will respond with, "ANYTHING ELSE I CAN HELP WITH?"
+If the customer (user) says, "GOODBYE!" a second time.
+  The the customer representative (program) will respond with, "THANK YOU FOR CALLING!"
+The customer representitive (program) then hangs up (exits)
